3c284f7a712e4f7dee8a3694efc939d3aff4ea40
Prashant Agrawal <pritamtg99999@gmail.com>
2024-12-25 13:57:24 +0530

    second

--- main.py
+++ main.py
@@ -11,23 +11,21 @@ turtle.shape(image)
 
 game_is_on = True
 data = pd.read_csv("50_states.csv")
-# print(data)
+guessed_state = []
 
-count = 0
-while game_is_on:
-    # count += 1
-    user_input = screen.textinput(f"Counter {count} / {STATES} States Correct", prompt="What's another state name?")
+while len(guessed_state) < 50:
+    user_input = screen.textinput(f"Counter {len(guessed_state)} / {STATES} States Correct",
+                                  prompt="What's another state name?").title()
+    print(user_input)
+    all_states = data["state"].to_list()
+    if user_input in all_states:
+        guessed_state.append(user_input)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == user_input]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(user_input)
 
-    for state in data["state"]:
-        if user_input.lower() == state.lower():
-            t = turtle.Turtle()
-            t.hideturtle()
-            t.penup()
-            state_data = data[data["state"] == user_input]
-            t.goto(int(state_data["x"]), int(state_data["y"]))
-            t.write(state_data["state"])
-            # count += 1
-            # print(user_input)
-            # game_is_on = False
 
 screen.exitonclick()
