1db71290d5f6b4f6200314afd874a0c8e5ea3528
Prashant Agrawal <pritamtg99999@gmail.com>
2024-12-28 17:08:01 +0530

    list comprehension in missed_state[]

--- main.py
+++ main.py
@@ -19,10 +19,7 @@ while len(guessed_state) < 50:
     all_states = data["state"].to_list()
 
     if user_input == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_state:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_state]
 
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("States Game Sheet ")
